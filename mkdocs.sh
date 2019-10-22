e9149c10c985a2208e7564faf7d1918d83515b30
Pavel Potocek <pavelpotocek@gmail.com>
2019-10-22 07:18:34 +0200

    Update the doc script

--- mkdocs.sh
+++ mkdocs.sh
@@ -1,8 +1,9 @@
-pdoc --html concur
-mv html html_
-git checkout gh-pages
-cp -ru html_/concur html/
-rm -r html_
-git commit -am 'Update documentation'
+pdoc --html concur && \
+mv html html_ && \
+git checkout gh-pages && \
+cp -ru html_/concur html/ && \
+rm -r html_ && \
+git add html/concur && \
+git commit -am 'Update documentation' && \
 git push
 git checkout master
