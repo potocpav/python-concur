03ec966998af0c4348ddd7f5754f00bd8b858c8b
Pavel Potocek <pavelpotocek@gmail.com>
2019-10-22 07:47:30 +0200

    Put docs into a docs forlder, not a branch

--- mkdocs.sh
+++ mkdocs.sh
@@ -1,10 +1,3 @@
-# This doesn't work yet
-pdoc --html concur && \
-    mv html html_ && \
-    git checkout gh-pages && \
-    cp -r html_/concur html/ && \
-    rm -r html_ && \
-    git add html/concur && \
-    git commit -am 'Update documentation' && \
-    git push
-git checkout master
+pdoc --html  --force --output-dir docs concur
+mv docs/concur/* docs
+rmdir docs/concur
