1a0dadd3fc64f5b62022ef42c029948a4ab5e4a2
Pavel Potocek <pavelpotocek@gmail.com>
2019-10-22 07:23:49 +0200

    Add a comment to mkdocs

--- mkdocs.sh
+++ mkdocs.sh
@@ -1,9 +1,10 @@
+# This doesn't work yet
 pdoc --html concur && \
-mv html html_ && \
-git checkout gh-pages && \
-cp -ru html_/concur html/ && \
-rm -r html_ && \
-git add html/concur && \
-git commit -am 'Update documentation' && \
-git push
+    mv html html_ && \
+    git checkout gh-pages && \
+    cp -r html_/concur html/ && \
+    rm -r html_ && \
+    git add html/concur && \
+    git commit -am 'Update documentation' && \
+    git push
 git checkout master
