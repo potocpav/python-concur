c566013a04179d68b56be4a477bd26a6d6de6595
Pavel Potoček <pavelpotocek@gmail.com>
2019-10-23 11:34:42 +0200

    Simplify the timers example

--- examples/timers.py
+++ examples/timers.py
@@ -2,7 +2,6 @@
 
 import time
 from concurrent.futures import ThreadPoolExecutor
-
 import concur as c
 
 
@@ -10,16 +9,14 @@ executor = ThreadPoolExecutor()
 
 
 def timer():
-    while True:
-        yield from c.orr([c.text(""), c.button("Start timer")])
-        yield
-        future = executor.submit(lambda: time.sleep(3))
-        yield from c.orr([c.text("waiting for 3s..."), c.button("Cancel"), c.Block(future)])
-        yield
+    yield from c.orr([c.text(""), c.button("Start timer")])
+    yield
+    future = executor.submit(lambda: time.sleep(3))
+    yield from c.orr([c.text("waiting for 3s..."), c.button("Cancel"), c.Block(future)])
 
 
 def app():
-    return c.orr([timer() for _ in range(5)])
+    return c.orr([c.forever(timer) for _ in range(5)])
 
 
 if __name__ == "__main__":
