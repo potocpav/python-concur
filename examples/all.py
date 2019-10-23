552f52baaf35bda0450210b0c13221bacf176971
Pavel Potoček <pavelpotocek@gmail.com>
2019-10-23 11:30:57 +0200

    Add keypress example to the "all" showcase

--- examples/all.py
+++ examples/all.py
@@ -1,16 +1,12 @@
 #!/usr/bin/env python3
 
 import concur as c
-
-import counters
-import hello_world
-import timers
-import todo
+import counters, hello_world, timers, todo, keypress
 
 
 def app():
     return c.orr([c.window(module.__name__, [module.app()]) for module in
-        [ counters, hello_world, timers, todo]])
+        [ counters, hello_world, timers, todo, keypress]])
 
 
 if __name__ == "__main__":
