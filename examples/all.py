76f3b13fd65cc5893c56ba6a74c71177f713d183
Pavel Potocek <pavelpotocek@gmail.com>
2019-10-28 21:11:35 +0100

    Sort examples alphabetically

--- examples/all.py
+++ examples/all.py
@@ -6,7 +6,7 @@ import counters, hello_world, image, keypress, timers, todo
 
 def app():
     return c.orr([c.window(module.__name__, [module.app()]) for module in
-        [ counters, hello_world, image, timers, todo, keypress]])
+        [ counters, hello_world, image, keypress, timers, todo,]])
 
 
 if __name__ == "__main__":
