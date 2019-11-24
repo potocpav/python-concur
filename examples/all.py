c9b4dda82b6debab062a549b41a1e18e1bf61b19
Pavel Potocek <pavelpotocek@gmail.com>
2019-11-24 10:50:55 +0100

    Add forked imgui installation instructions

--- examples/all.py
+++ examples/all.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
 
 import concur as c
-import imgui
 import counters, hello_world, image, style, timers, todo
 
 
 def app():
     return c.orr([c.window(module.__name__, module.app()) for module in
-        [ counters, hello_world, image, style, timers, todo,]])
+        [counters, hello_world, image, style, timers, todo]])
 
 
 if __name__ == "__main__":
