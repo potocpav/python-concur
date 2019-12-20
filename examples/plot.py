f79696d7e28f6d2b516e313cc4b5222f0d44f817
Pavel Potoček <pavel.potocek@certicon.cz>
2019-12-20 13:25:05 +0100

    Handle keep_aspect correctly for flipped axes

--- examples/plot.py
+++ examples/plot.py
@@ -5,7 +5,7 @@ from PIL import Image
 import numpy as np
 
 
-x = np.linspace(0, 10, 100000, dtype=np.float32)
+x = np.linspace(0, 10, 10000, dtype=np.float32)
 y = np.sin(x) * np.sin(x * 5 * np.cos(x))
 pts = np.stack([x, y]).T
 
@@ -19,7 +19,7 @@ def graph(tf):
 
 def app():
     image = Image.open("examples/lenna.png")
-    view = c.plot.Frame((0, 2), (10, -2), keep_aspect=False, fix_axis=None)
+    view = c.plot.Frame((0, 2), (10, -2), keep_aspect=True, fix_axis=None)
     while True:
         tag, value = yield from c.orr(
             [ c.text("Drag using right mouse button,\nscroll using mouse wheel.")
