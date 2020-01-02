40e7f228df449aba10ababb576fb6460cc754b4f
Pavel Potocek <pavelpotocek@gmail.com>
2020-01-02 22:47:56 +0100

    Show testing cursor

--- concur/testing.py
+++ concur/testing.py
@@ -5,6 +5,8 @@ Must be used in conjunction with the `concur.integrations.puppet` backend.
 
 import imgui
 from concur.integrations.puppet import PuppetRenderer, main
+from concur.draw import polyline
+from concur.core import orr
 
 
 def test(widget_gen, slow=False):
@@ -13,6 +15,13 @@ def test(widget_gen, slow=False):
         500,
         500)
 
+
+def show_cursor():
+    io = imgui.get_io()
+    while True:
+        x, y = io.mouse_pos
+        next(polyline([(x, y), (x, y+20), (x+10, y+15)], (1,1,1,1), True, 2))
+        yield
 
 class Testing(object):
     def __init__(self, puppet_renderer, slow):
@@ -24,17 +33,18 @@ class Testing(object):
         self.puppet.click(button)
 
     def click_next(self):
-        yield from self.slowdown()
-        yield
-        origin = imgui.get_cursor_screen_pos()
-        self.puppet.set_mouse_pos(*origin)
-        yield from self.slowdown()
-        yield
-        self.puppet.click()
-        while True:
-            yield
+        def body():
+            yield from self.pause()
+            x, y = imgui.get_cursor_screen_pos()
+            self.puppet.set_mouse_pos(x+5, y+5)
+            yield from self.pause()
+            self.puppet.click()
+            while True:
+                yield
+        return orr([show_cursor(), body()])
 
-    def slowdown(self):
+    def pause(self):
         if self.slow:
             for i in range(30):
                 yield
+        yield
