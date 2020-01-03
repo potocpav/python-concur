7c15d3ac97738591428cb1d8914b3ed521afea52
Pavel Potoček <pavel.potocek@certicon.cz>
2020-01-03 19:43:51 +0100

    Add a testing polyline example

---
+++ examples/polyline.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+import concur as c
+from PIL import Image
+import numpy as np
+import imgui
+
+def graph(tex_id, tex_w, tex_h, tf):
+    return c.orr(
+        [ c.draw.polyline([(-1, 0), (0, 0), (0, -0.3), (0.3, 0), (1, 0.1), (-1, 0.5), (-1, 0.2)], (0,0,0,0.5), closed=True, thickness=20, tf=tf)
+        # , c.draw.polyline(np.random.rand(100000,2), (0,0,1,1), thickness=4, tf=tf)
+        ])
+
+
+def app():
+    view = c.plot.Frame((-1, -1), (1, 1), keep_aspect=True)
+    arr = np.array(Image.open("examples/lenna.png"))
+    tex = c.texture(arr)
+    style = imgui.get_style()
+    style.anti_aliased_lines = False
+
+    while True:
+        tag, value = yield from c.orr(
+            [ c.window("Graph", c.plot.frame("Frame", view, c.partial(graph, tex, arr.shape[0], arr.shape[1])))
+            , c.window("Controls", c.checkbox("Antialiasing", style.anti_aliased_lines))
+            ])
+        if tag == "Frame":
+            view = value
+        elif tag == "Antialiasing":
+            style.anti_aliased_lines = value
+        yield
+
+
+if __name__ == "__main__":
+    c.integrations.main("Plot", app(), 800, 500)
