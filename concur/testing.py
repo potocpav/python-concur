5b3a233300be6c3d5f4f844a8d646ca892f5fdd6
Pavel Potoček <pavel.potocek@certicon.cz>
2020-01-03 11:42:21 +0100

    Add more mouse testing capability, simplify tests by using decorators, add an image test

--- concur/testing.py
+++ concur/testing.py
@@ -2,9 +2,10 @@
 
 import os
 import imgui
+import numpy as np # for floating point ranges
 from concur.integrations.puppet import PuppetRenderer, main
 from concur.draw import polyline
-from concur.core import orr
+from concur.core import orr, optional
 
 
 def test(widget_gen, slow=False):
@@ -14,34 +15,83 @@ def test(widget_gen, slow=False):
         500)
 
 
-def show_cursor():
+def test_widget(f):
+    """ Function decorator for testing functions.
+
+    See `concur.testing.test_widgets` for usage examples.
+    """
+    def g():
+        return test(lambda tester: orr(
+            [ optional(tester.slow or True, draw_cursor)
+            , f(tester)
+            ]))
+    return g
+
+
+def draw_cursor():
+    """ Polygonal line or a closed polygon. """
     io = imgui.get_io()
-    while True:
+    while(True):
+        draw_list = imgui.get_overlay_draw_list()
         x, y = io.mouse_pos
-        next(polyline([(x, y), (x, y+20), (x+10, y+15)], (1,1,1,1), True, 2))
+        pts = [(x, y), (x, y+20), (x+1, y+20), (x+6, y+15), (x+13, y+15), (x+14, y+14), (x+1, y)]
+        color = imgui.get_color_u32_rgba(1-io.mouse_down[1],1-io.mouse_down[0],io.mouse_down[2],1)
+        draw_list.add_polyline(pts, color, True, 2)
         yield
+
 
 class Testing(object):
     """ Must be used in conjunction with the `concur.integrations.puppet` backend. """
     def __init__(self, puppet_renderer, slow):
         assert isinstance(puppet_renderer, PuppetRenderer)
         self.puppet = puppet_renderer
-        self.slow = 'SLOW_TEST' in os.environ
-        print(self.slow)
+        self.slow = 'SLOW_TEST' in os.environ and os.environ['SLOW_TEST'] == '1'
 
     def click(self, button=0):
+        "Click a given mouse button"
         self.puppet.click(button)
 
     def click_next(self):
-        def body():
-            yield from self.pause()
-            x, y = imgui.get_cursor_screen_pos()
-            self.puppet.set_mouse_pos(x+5, y+5)
-            yield from self.pause()
-            self.puppet.click()
-            while True:
+        "Click the next widget"
+        x, y = imgui.get_cursor_screen_pos()
+        yield from self.move_cursor(x+5, y+5)
+        yield from self.pause()
+        self.puppet.click()
+        while True:
+            yield
+
+    def move_cursor(self, x, y):
+        "Move cursor to a given position"
+        io = imgui.get_io()
+        ox, oy = io.mouse_pos
+        yield
+        if self.slow:
+            for f in np.linspace(0, 1, 30):
+                self.puppet.set_mouse_pos(x * f + ox * (1-f), y * f + oy * (1-f))
                 yield
-        return orr([show_cursor(), body()])
+        else:
+            self.puppet.set_mouse_pos(x, y)
+            yield
+
+    def scroll_up(self):
+        "Scroll up"
+        self.puppet.scroll_up()
+        yield
+
+    def scroll_dn(self):
+        "Scroll down"
+        self.puppet.scroll_dn()
+        yield
+
+    def mouse_up(self, button=0):
+        "Mouse up"
+        self.puppet.mouse_up(button)
+        yield
+
+    def mouse_dn(self, button=0):
+        "Mouse down"
+        self.puppet.mouse_dn(button)
+        yield
 
     def pause(self):
         if self.slow:
--- tests/test_widgets.py
+++ tests/test_widgets.py
@@ -1,36 +1,31 @@
-#!/usr/bin/env python3
-
 import concur as c
-import pytest
 
 
-def test_button():
-    def widget(tester):
-        res = yield from c.orr(
-            [ tester.click_next()
-            , c.button("This")
-            ])
-        assert res == ("This", None)
-        yield
-        res = yield from c.orr(
-            [ tester.click_next()
-            , c.button("That", tag="Tag")
-            ])
-        assert res == ("Tag", None)
-    c.testing.test(widget)
+@c.testing.test_widget
+def test_button(tester):
+    res = yield from c.orr(
+        [ tester.click_next()
+        , c.button("This")
+        ])
+    assert res == ("This", None)
+    yield
+    res = yield from c.orr(
+        [ tester.click_next()
+        , c.button("That", tag="Tag")
+        ])
+    assert res == ("Tag", None)
 
 
-def test_checkbox():
-    def widget(tester):
-        res = yield from c.orr(
-            [ tester.click_next()
-            , c.checkbox("This", True)
-            ])
-        assert res == ("This", False)
-        yield
-        res = yield from c.orr(
-            [ tester.click_next()
-            , c.checkbox("That", False, tag="Tag")
-            ])
-        assert res == ("Tag", True)
-    c.testing.test(widget)
+@c.testing.test_widget
+def test_checkbox(tester):
+    res = yield from c.orr(
+        [ tester.click_next()
+        , c.checkbox("This", True)
+        ])
+    assert res == ("This", False)
+    yield
+    res = yield from c.orr(
+        [ tester.click_next()
+        , c.checkbox("That", False, tag="Tag")
+        ])
+    assert res == ("Tag", True)
