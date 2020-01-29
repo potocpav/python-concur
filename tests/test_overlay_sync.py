3a31403011ff13741257069f93569dcedee1ef42
Pavel Potoček <pavel.potocek@certicon.cz>
2020-01-29 13:49:34 +0100

    Cleanup the sync test

--- tests/test_overlay_sync.py
+++ tests/test_overlay_sync.py
@@ -1,21 +1,20 @@
 import concur as c
 import numpy as np
 
+
 @c.testing.test_widget
 def app(tester):
     canvas = np.zeros([1000, 1000, 3], 'u1')
     im = c.Image(canvas)
     t = 0
     x, y = 0, 0
-    for _ in range(5):
-        yield
+    for i in range(2):
+        if i: yield
         res = yield from c.orr(
             [ c.image("", im, content_gen=lambda tf:
                 c.draw.rect_filled(x-40, y-40, x+40, y+40, np.array([222, 111, 111, 255]) / 255, tf=tf))
             , c.event(None)
             ])
-        if res == "Stop":
-            break
         t += 0.02
         x = int(500 + 400 * np.sin(t))
         y = int(500 + 400 * np.cos(t))
