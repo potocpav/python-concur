23fbb8c3abb42ea3d517a18a707b255bdfb24020
Pavel Potocek <pavelpotocek@gmail.com>
2020-02-10 20:35:37 +0100

    Make it possible to control tesing in an external widget. Update homepage.

--- concur/testing.py
+++ concur/testing.py
@@ -3,14 +3,15 @@
 import os
 import imgui
 import numpy as np # for floating point ranges
+from functools import partial
 from concur.integrations.puppet import PuppetRenderer, main
 from concur.draw import polyline
 from concur.core import orr, optional
 
 
-def test(widget_gen, slow=False, width=512, height=512, *args, **argv):
+def test(widget_gen, slow=False, draw_cursor=True, width=512, height=512, *args, **argv):
     return main("Automatic Tester",
-        lambda puppet_renderer: widget_gen(Testing(puppet_renderer, slow)),
+        lambda puppet_renderer: widget_gen(draw_cursor, Testing(puppet_renderer, slow)),
         width, height,
         *args, **argv)
 
@@ -20,12 +21,13 @@ def test_widget(f):
 
     See `concur.testing.test_widget` for usage examples.
     """
-    def widget(tester):
+    def widget_gen(draw_cursor, tester):
         io = imgui.get_io()
-        io.mouse_draw_cursor = True
+        io.mouse_draw_cursor = draw_cursor
         yield from f(tester)
     def g(*args, **argv):
-        return test(widget, *args, **argv)
+        draw_cursor = 'draw_cursor' in argv and argv['draw_cursor']
+        return test(widget_gen, *args, **argv)
     return g
 
 
@@ -35,19 +37,46 @@ class Testing(object):
         assert isinstance(puppet_renderer, PuppetRenderer)
         self.puppet = puppet_renderer
         self.slow = 'SLOW_TEST' in os.environ and os.environ['SLOW_TEST'] == '1'
+        self.marked = {}
 
     def click(self, button=0):
         "Click a given mouse button"
-        self.puppet.click(button)
+        self.puppet.mouse_dn(button)
+        if self.slow:
+            for i in range(10):
+                yield
+        yield
+        self.puppet.mouse_up(button)
 
     def click_next(self):
         "Click the next widget"
         x, y = imgui.get_cursor_screen_pos()
         yield from self.move_cursor(x+5, y+5)
         yield from self.pause()
-        self.puppet.click()
+        yield from self.click()
+        # Give the widget time to react
+        yield
+        yield
+
+    def mark(self, name, widget):
+        """ Display a widget, but mark it with a name so it can be interacted with at a later point
+        using methods such as click_marked. """
         while True:
+            self.marked[name] = imgui.get_cursor_screen_pos()
+            try:
+                next(widget)
+            except StopIteration as e:
+                return e.value
             yield
+
+    def click_marked(self, name, x=5, y=5):
+        "Click the given previously-marked widget. Optionally, specify the click offset x, y coords."
+        if name not in self.marked:
+            raise ValueError(f"Name '{name}' was not previously marked.")
+        x0, y0 = self.marked[name]
+        yield from self.move_cursor(x0 + x, y0 + y)
+        yield from self.pause()
+        yield from self.click()
 
     def move_cursor(self, x, y):
         "Move cursor to a given position"
@@ -61,6 +90,10 @@ class Testing(object):
         else:
             self.puppet.set_mouse_pos(x, y)
             yield
+
+    def set_mouse_pos(self, x, y):
+        "Set mouse position instantly."
+        self.puppet.set_mouse_pos(x, y)
 
     def scroll_up(self):
         "Scroll up"
@@ -82,8 +115,17 @@ class Testing(object):
         self.puppet.mouse_dn(button)
         yield
 
-    def pause(self):
-        if self.slow:
-            for i in range(30):
+    def pause(self, nframes=0):
+        """Pause for a specified number of frames.
+
+        If nframs <= 0, the pause length depends on the
+        environment variable TEST_SLOW.
+        """
+        if nframes <= 0:
+            if self.slow:
+                for _ in range(30):
+                    yield
+            yield
+        else:
+            for _ in range(nframes):
                 yield
-        yield
---
+++ gen_screenshots.py
@@ -0,0 +1,110 @@
+
+import sys
+import os
+import concur as c
+import numpy as np
+from PIL import Image
+
+examples_path = os.path.join(sys.path[0], "examples")
+sys.path.append(examples_path)
+import all
+
+
+@c.testing.test_widget
+def all_examples(tester):
+    return c.orr([all.app(), tester.pause()])
+
+
+@c.testing.test_widget
+def hello_world(tester):
+    return c.orr([c.text("Hello, world!"), tester.pause()])
+
+
+@c.testing.test_widget
+def counter(tester):
+    def controlee():
+        counter = 5
+        while True:
+            action, _ = yield from c.orr([
+            c.text(f"Count: {counter}"),
+            tester.mark("--", c.button("Decrement")),
+            tester.mark("++", c.button("Increment")),
+            ])
+            if action == "Increment":
+                counter += 1
+            elif action == "Decrement":
+                counter -= 1
+            yield
+    def controller():
+        yield from tester.pause()
+        yield from tester.click_marked("++", 40, 10)
+        yield from tester.click_marked("--", 30, 10)
+        yield from tester.click_marked("--", 30, 10)
+        yield from tester.click_marked("++", 40, 10)
+        yield from tester.move_cursor(100, 100)
+    return c.orr([controlee(), controller()])
+
+
+@c.testing.test_widget
+def image(tester):
+    def controlee():
+        view = c.Image(Image.open("examples/lenna.png"))
+        while True:
+            _, view = yield from c.image("Image", view)
+            yield
+    def controller():
+        yield from tester.pause(30)
+        yield from tester.move_cursor(200, 150)
+        yield from tester.scroll_up()
+        yield from tester.pause(10)
+        yield from tester.scroll_up()
+        yield from tester.pause(10)
+        yield from tester.scroll_up()
+        yield from tester.move_cursor(210, 150)
+        yield from tester.scroll_up()
+        yield from tester.pause(10)
+        yield from tester.scroll_up()
+        yield from tester.pause(10)
+        yield from tester.scroll_up()
+        yield from tester.pause(10)
+        yield from tester.scroll_up()
+        yield from tester.pause(30)
+        yield from tester.move_cursor(210, 200)
+
+        yield from tester.mouse_dn(1)
+        np.random.seed(0)
+        for _ in range(5):
+            yield from tester.move_cursor(np.random.randint(100, 200), np.random.randint(100, 200))
+        yield from tester.move_cursor(210, 200)
+        yield from tester.mouse_up(1)
+
+        yield from tester.move_cursor(210, 150)
+        yield from tester.pause(30)
+        yield from tester.scroll_dn()
+        yield from tester.pause(10)
+        yield from tester.scroll_dn()
+        yield from tester.pause(10)
+        yield from tester.scroll_dn()
+        yield from tester.pause(10)
+        yield from tester.scroll_dn()
+        yield from tester.move_cursor(200, 150)
+        yield from tester.scroll_dn()
+        yield from tester.pause(10)
+        yield from tester.scroll_dn()
+        yield from tester.pause(10)
+        yield from tester.scroll_dn()
+        yield from tester.move_cursor(100, 100)
+
+    return c.orr([controlee(), controller()])
+
+
+if __name__ == "__main__":
+    im = all_examples(width=800, height=560, return_sshot=True, draw_cursor=False)
+    Image.fromarray(im[...,:3]).save('screenshot.png')
+
+    im = hello_world(width=400, height=288, return_sshot=True, draw_cursor=False)
+    Image.fromarray(im[...,:3]).save('docs/sshots/hello_world.png')
+
+    counter(width=400, height=288, draw_cursor=True, save_screencast='docs/sshots/counter.mp4')
+
+    image(width=400, height=288, draw_cursor=True, save_screencast='docs/sshots/image.mp4')
