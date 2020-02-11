f3828f30299a46535a51802be5144b1217ce86e9
Pavel Potocek <pavelpotocek@gmail.com>
2020-02-11 16:23:52 +0100

    Remove time_controls

--- concur/extra_widgets/__init__.py
+++ concur/extra_widgets/__init__.py
@@ -1,10 +1,8 @@
 """ Extra widgets that do not directly correspond to ImGui calls.
 """
 
-__pdoc__ = dict(time_controls=False)
 
 from .frame import *
-from .time_controls import *
 from .image import *
 from .pan_zoom import *
 from .draggable import *
