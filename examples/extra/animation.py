e738f5892cbecf5ff1195767903913b7546f7884
Pavel Potocek <pavelpotocek@gmail.com>
2020-02-11 14:27:37 +0100

    Add an animation example

--- examples/extra/animation.py
+++ examples/extra/animation.py
@@ -2,16 +2,12 @@
 
 import concur as c
 import numpy as np
-import time
-
+from time import monotonic as t
 
 def graph(tf):
-    t = time.monotonic()
-    ts = np.linspace(t, t + 1/2, 100)
-    x = np.sin(ts * 4)
-    y = np.cos(ts * 5)
-    return c.draw.polyline(np.stack([x, y]).T, (0,0,1,1), thickness=2, tf=tf)
-
+    ts = np.linspace(t(), t() + 1/2, 100)
+    pts = np.stack([np.sin(ts * 4), np.cos(ts * 5)]).T
+    return c.draw.polyline(pts, (0,0,1,1), tf=tf)
 
 def app():
     view = c.plot.Frame((-1.5, 1.5), (1.5, -1.5))
@@ -24,6 +20,5 @@ def app():
             view = value
         yield
 
-
 if __name__ == "__main__":
-    c.main("Plot", app(), 500, 500)
+    c.main("Plot", app(), 400, 288)
