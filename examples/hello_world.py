4366fb346ef815420de4491035410ce73f8db895
Pavel Potoček <pavel.potocek@certicon.cz>
2020-06-15 15:00:31 +0200

    These changes are experimental and not to be merged.

--- examples/hello_world.py
+++ examples/hello_world.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python3
 
 import concur as c
+import imgui
 
 
 def app():
-    return c.button("Close")
+    # make_next_win_fullscreen = c.lift(lambda: imgui.set_next_window_size(*imgui.get_io().display_size))
+    return c.orr([
+        c.window("Title", c.orr([
+            c.button("Close"),
+            ])),
+        ])
 
 
 if __name__ == "__main__":
